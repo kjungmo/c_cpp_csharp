489f3f0d8aadb628d891255550f0186e59919cd5
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-27 17:36:51 +0900

    주석 추가

--- pointer/pointer/pointer.c
+++ pointer/pointer/pointer.c
@@ -2,12 +2,12 @@
 
 void main(void)
 {
-	int nData = 10;
-	int *pnData = &nData;
+	int nData = 10; // 정수형 데이터 선언 및 초기화
+	int *pnData = &nData; // 포인터 변수 선언, nData의 주소로 초기화
 
-	printf("%d, %d\n", nData, *pnData);
-	printf("%p, %p\n", &nData, pnData);
+	printf("%d, %d\n", nData, *pnData); // 간접 지정 연산을 통한 값 출력
+	printf("%p, %p\n", &nData, pnData); // 주소 출력
 
-	*pnData = 20;
+	*pnData = 20; // 
 	printf("%d\n", nData);
 }
