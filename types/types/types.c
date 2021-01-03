ab34230cc71ee2f1d3a8c22c2b82be93aef00761
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-03 17:46:11 +0900

    difference between float & double

--- types/types/types.c
+++ types/types/types.c
@@ -6,6 +6,20 @@ int main()
     int size;
 
     size = sizeof num;
-
     printf("size of num is : %d\n", size);
+
+    printf("char: %\d, short: %d, int: %d, long: %d, long long: %d, "
+    "float: %d, double: %d\n,12.3f: %d, 12.3: %d\n",
+        sizeof(char),
+        sizeof(short),
+        sizeof(int),
+        sizeof(long),
+        sizeof(long long),
+        sizeof(float),
+        sizeof(double),
+        sizeof(12.3f),
+        sizeof(12.3)
+    );
+
+    return 0;
 }
