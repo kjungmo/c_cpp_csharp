a54b04d1df3fdf737539ffef111f3f5aaf298f3a
kjungmo <kangjmo91@gmail.com>
2021-03-05 14:10:31 +0900

    changes in control components

--- LocalizationTesterA/LocalizationTesterA/Form1.Designer.cs
+++ LocalizationTesterA/LocalizationTesterA/Form1.Designer.cs
@@ -30,86 +30,41 @@ namespace LocalizationTesterA
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.korLang = new System.Windows.Forms.RadioButton();
-            this.engLang = new System.Windows.Forms.RadioButton();
-            this.gerLang = new System.Windows.Forms.RadioButton();
-            this.fraLang = new System.Windows.Forms.RadioButton();
-            this.japLang = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 369);
+            this.button1.Location = new System.Drawing.Point(335, 275);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "버튼1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(185, 407);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "버튼2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(185, 445);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "버튼3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(185, 484);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "버튼4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(22, 22);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(374, 292);
+            this.richTextBox1.Size = new System.Drawing.Size(216, 292);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(450, 338);
+            this.groupBox1.Location = new System.Drawing.Point(450, 93);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(212, 221);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "테스트";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(29, 100);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
@@ -121,114 +76,39 @@ namespace LocalizationTesterA
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 373);
+            this.label1.Location = new System.Drawing.Point(284, 279);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 7;
             this.label1.Text = "라벨1";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(450, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 8;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 414);
+            this.label2.Location = new System.Drawing.Point(400, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "라벨2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(134, 452);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "라벨3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(134, 491);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "라벨4";
-            // 
-            // korLang
-            // 
-            this.korLang.AutoSize = true;
-            this.korLang.Checked = true;
-            this.korLang.Location = new System.Drawing.Point(441, 22);
-            this.korLang.Name = "korLang";
-            this.korLang.Size = new System.Drawing.Size(61, 19);
-            this.korLang.TabIndex = 11;
-            this.korLang.TabStop = true;
-            this.korLang.Text = "한국어";
-            this.korLang.UseVisualStyleBackColor = true;
-            // 
-            // engLang
-            // 
-            this.engLang.AutoSize = true;
-            this.engLang.Location = new System.Drawing.Point(567, 22);
-            this.engLang.Name = "engLang";
-            this.engLang.Size = new System.Drawing.Size(63, 19);
-            this.engLang.TabIndex = 12;
-            this.engLang.TabStop = true;
-            this.engLang.Text = "English";
-            this.engLang.UseVisualStyleBackColor = true;
-            // 
-            // gerLang
-            // 
-            this.gerLang.AutoSize = true;
-            this.gerLang.Location = new System.Drawing.Point(441, 58);
-            this.gerLang.Name = "gerLang";
-            this.gerLang.Size = new System.Drawing.Size(69, 19);
-            this.gerLang.TabIndex = 13;
-            this.gerLang.TabStop = true;
-            this.gerLang.Text = "Deutsch";
-            this.gerLang.UseVisualStyleBackColor = true;
-            // 
-            // fraLang
-            // 
-            this.fraLang.AutoSize = true;
-            this.fraLang.Location = new System.Drawing.Point(567, 58);
-            this.fraLang.Name = "fraLang";
-            this.fraLang.Size = new System.Drawing.Size(61, 19);
-            this.fraLang.TabIndex = 14;
-            this.fraLang.TabStop = true;
-            this.fraLang.Text = "French";
-            this.fraLang.UseVisualStyleBackColor = true;
-            // 
-            // japLang
-            // 
-            this.japLang.AutoSize = true;
-            this.japLang.Location = new System.Drawing.Point(441, 98);
-            this.japLang.Name = "japLang";
-            this.japLang.Size = new System.Drawing.Size(61, 19);
-            this.japLang.TabIndex = 15;
-            this.japLang.TabStop = true;
-            this.japLang.Text = "日本語";
-            this.japLang.UseVisualStyleBackColor = true;
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "언어";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 586);
-            this.Controls.Add(this.japLang);
-            this.Controls.Add(this.fraLang);
-            this.Controls.Add(this.gerLang);
-            this.Controls.Add(this.engLang);
-            this.Controls.Add(this.korLang);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(687, 335);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "폼1";
@@ -242,22 +122,12 @@ namespace LocalizationTesterA
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RadioButton korLang;
-        private System.Windows.Forms.RadioButton engLang;
-        private System.Windows.Forms.RadioButton gerLang;
-        private System.Windows.Forms.RadioButton fraLang;
-        private System.Windows.Forms.RadioButton japLang;
     }
 }
 
