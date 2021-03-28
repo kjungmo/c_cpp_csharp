2a1c1a1551a6db5f3aa30fc28a2df741c1f00c12
kjungmo <kangjmo91@gmail.com>
2021-03-28 21:19:15 +0900

    adds Log managing codes

--- LocalizationTesterD/Program.cs
+++ LocalizationTesterD/Program.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
 
 namespace LocalizationTesterD
 {
     static class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
-        [STAThread]
+    /// <summary>
+    /// 해당 애플리케이션의 주 진입점입니다.
+    /// </summary>
+    [STAThread]
         static void Main()
         {
+
+            Application.ThreadException += Application_ThreadException;
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.Automatic);
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            throw new Exception("UI 생성 전 Exception throw.");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Trace.WriteLine($"CurrentDomain_UnhandledException " +
+                $"{((Exception)e.ExceptionObject).Message}");
+            MessageBox.Show($"CurrentDomain_UnhandledException " +
+                $"{((Exception)e.ExceptionObject).Message} Is Terminating: {e.IsTerminating.ToString()}");
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            Trace.WriteLine($"Application_ThreadException {e.Exception.Message}");
+            MessageBox.Show($"Application_ThreadException {e.Exception.Message}");
+            Application.Exit(new System.ComponentModel.CancelEventArgs(false));
+        }
+
     }
+
 }
---
+++ LocalizationTesterD/Tools/LogManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LocalizationTesterD.Tools
+{
+    public class LogManager
+    {
+        private string _path;
+
+
+        #region Constructors
+        public LogManager(string path)
+        {
+            _path = path;
+            _SetLogPath();
+        }
+
+        public LogManager()
+            :this(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log"))
+        {
+
+        }
+        #endregion
+
+
+        #region Methods 
+        private void _SetLogPath()
+        {
+            if (Directory.Exists(_path))
+                Directory.CreateDirectory(_path);
+
+            string logFile = DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            _path = Path.Combine(_path, logFile);
+
+        }
+
+        public void Write(string data)
+        {
+
+        }
+
+        public void WriteLine(string data)
+        {
+
+        }
+        #endregion
+    }
+}
