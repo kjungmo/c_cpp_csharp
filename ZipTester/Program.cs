2b8465398309f76bec568974925e0982cbf0da6e
kjungmo <kangjmo91@gmail.com>
2021-04-04 20:40:32 +0900

    added switch statements to select modes

--- ZipTester/Program.cs
+++ ZipTester/Program.cs
@@ -10,31 +10,57 @@ namespace ZipTester
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("CogAplex Log Management System\r");
-            Console.WriteLine("------------------------------\n");
 
-            Console.Write("Log Directory : ");
-            string logDir = Console.ReadLine();
-            Console.Write("Zipfile Name : ");
-            string zipName = Console.ReadLine();
-            CompressZipByIO(logDir, MakeZipDir(logDir, zipName));
-            Console.Write("Delete Compressed Files? (Y/N) : ");
+            
+            Console.WriteLine("CogAplex Log File Management System\r");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("<<<  Modes  >>>   ZIP   ||  UNZIP ");
+            Console.WriteLine("[ZIP MODE] [Directory] [File Extension Type] [Interval] ");
+            Console.WriteLine("[UNZIP MODE] [Directory]");
 
-            switch(Console.ReadLine())
+            string[] arguments = Environment.GetCommandLineArgs();
+            arguments = arguments.Where(condition => condition != arguments[0]).ToArray();
+            foreach (string arg in arguments)
             {
-                case "y":
-                case "Y":
-                    DeleteFile(logDir);
-                    break;
-                case "n":
-                case "N":
-                    Console.WriteLine("Files Left.");
-                    break;
-
-
+                Console.WriteLine(arg);
             }
+
+            //ZipTester.exe zip f:\Github\ .log/.jpg daily
+
+            switch (arguments[0].ToLower())
+            {
+                case "zip":
+                    Console.WriteLine("Zip Mode Selected!");
+                    break;
+                case "unzip":
+                    Console.WriteLine("Unzip Mode Selected!");
+                    break;
+                default:
+                    break;
+            }
+            //Console.Write("Log Directory : ");
+            //string logDir = Console.ReadLine();
+            //Console.Write("Zipfile Name : ");
+            //string zipName = Console.ReadLine();
+            //CompressZipByIO(logDir, MakeZipDir(logDir, zipName));
+            //Console.Write("Delete Compressed Files? (Y/N) : ");
+
+            //switch(Console.ReadLine())
+            //{
+            //    case "y":
+            //    case "Y":
+            //        DeleteFile(logDir);
+            //        break;
+            //    case "n":
+            //    case "N":
+            //        Console.WriteLine("Files Left.");
+            //        break;
+
+
+            //}
 
             Console.WriteLine("press any key...");
             Console.ReadKey();
