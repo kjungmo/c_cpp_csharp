ab1dc48f412a29445199429787b89932160d243e
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-06-04 11:21:18 +0900

    adds dummy jpg generator

--- DummyFileCreator/Program.cs
+++ DummyFileCreator/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,8 +18,9 @@ namespace DummyFileCreator
 			string logPath = @"E:\ZipTest\LOG";
 			string ngPath = @"E:\ZipTest\NG";
 			string okPath = @"E:\ZipTest\OK";
-			if (Directory.Exists(logPath))
+			if (!Directory.Exists(logPath))
 			{
+				Directory.CreateDirectory(logPath);
 				for (int i = 0; i > dates; i--)
 				{
 					string datetime = DateTime.Today.AddDays(i).ToString("yyyy-MM-dd");
@@ -27,34 +31,82 @@ namespace DummyFileCreator
 				Console.WriteLine("LOG fertig");
 			}
 
-			if (Directory.Exists(ngPath))
+			if (!Directory.Exists(ngPath))
 			{
-				for (int i = 0; i > dates; i--)
-				{
-					string datetime = DateTime.Today.AddDays(i).ToString("yyyyMMdd");
-					string folderpath = Path.Combine(ngPath, datetime);
-					if (!Directory.Exists(folderpath))
-					{
-						Directory.CreateDirectory(folderpath);
-					}
-				}
-				Console.WriteLine("NG fertig");
+				Directory.CreateDirectory(ngPath);
 			}
+			for (int i = 0; i > dates; i--)
+			{
+				string datetime = DateTime.Today.AddDays(i).ToString("yyyyMMdd");
+				string folderpath = Path.Combine(ngPath, datetime);
+				string carPath = "CN7";
+				carPath = Path.Combine(folderpath, carPath);
+				if (!Directory.Exists(folderpath))
+				{
+					Directory.CreateDirectory(folderpath);
+				}
+				if (!Directory.Exists(carPath))
+				{
+					Directory.CreateDirectory(carPath);
+				}
+				for (int j = 0; j < 15; j++)
+				{
+					GenerateDummyJpegAt(Path.Combine(carPath, j.ToString() + ".jpg"), "Captured Window", 1920, 1060);
+				}
+			}
+			Console.WriteLine("NG fertig");
 
-			if (Directory.Exists(okPath))
+			if (!Directory.Exists(okPath))
 			{
-				for (int i = 0; i > dates; i--)
-				{
-					string datetime = DateTime.Today.AddDays(i).ToString("yyyyMMdd");
-					string folderpath = Path.Combine(okPath, datetime);
-					if (!Directory.Exists(folderpath))
-					{
-						Directory.CreateDirectory(folderpath);
-					}
-				}
-				Console.WriteLine("OK fertig");
+				Directory.CreateDirectory(okPath);
 			}
+			for (int i = 0; i > dates; i--)
+			{
+				string datetime = DateTime.Today.AddDays(i).ToString("yyyyMMdd");
+				string folderpath = Path.Combine(ngPath, datetime);
+				string carPath = "CN7";
+				carPath = Path.Combine(folderpath, carPath);
+				if (!Directory.Exists(folderpath))
+				{
+					Directory.CreateDirectory(folderpath);
+				}
+				if (!Directory.Exists(carPath))
+				{
+					Directory.CreateDirectory(carPath);
+				}
+				for (int j = 0; j < 15; j++)
+				{
+					GenerateDummyJpegAt(Path.Combine(carPath, j.ToString() + ".jpg"), "Captured Window", 1920, 1060);
+				}
+			}
+			Console.WriteLine("OK fertig");
 			Console.ReadKey();
+		}
+
+		private static void GenerateDummyJpegAt(string outputPath, string nameToEmbed, int width, int height)
+		{
+			using (var bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+			{
+				BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+				byte[] noise = new byte[data.Width * data.Height * 3];
+				new Random().NextBytes(noise); // note that if you do that in a loop or from multiple threads - you may want to store this random in outside variable
+				Marshal.Copy(noise, 0, data.Scan0, noise.Length);
+				bmp.UnlockBits(data);
+				using (var g = Graphics.FromImage(bmp))
+				{
+					// draw white rectangle in the middle
+					g.FillRectangle(Brushes.White, 0, height / 2 - 20, width, 40);
+					var fmt = new StringFormat();
+					fmt.Alignment = StringAlignment.Center;
+					fmt.LineAlignment = StringAlignment.Center;
+					// draw text inside that rectangle
+					g.DrawString(nameToEmbed, SystemFonts.DefaultFont, Brushes.Black, new RectangleF(0, 0, bmp.Width, bmp.Height), fmt);
+				}
+				using (var fs = File.Create(outputPath))
+				{
+					bmp.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+				}
+			}
 		}
 	}
 }
