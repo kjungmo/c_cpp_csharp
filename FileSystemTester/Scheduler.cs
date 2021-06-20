ec440149634f7008f870bfe7d6f541cd2dc6c128
kjungmo <kangjmo91@gmail.com>
2021-06-20 22:25:14 +0900

    Update Scheduler.cs

--- FileSystemTester/Scheduler.cs
+++ FileSystemTester/Scheduler.cs
@@ -13,41 +13,12 @@ namespace LogManagementSystem
             MONTHLY,
         }
 
-        public static void AddTaskSchedule(string selectedInterval, string arguments, bool stopFlag = true)
+        public static void AddTaskSchedule(string arguments, Trigger startTrigger, bool stopFlag = true)
         {
             TaskDefinition taskDefinition = TaskService.Instance.NewTask();
             taskDefinition.RegistrationInfo.Author = "cogaplex@cogaplex.com";
             taskDefinition.RegistrationInfo.Description = "Management of daily .log and captured image files.";
-
-            ExeInterval interval;
-            if (!Enum.TryParse<ExeInterval>(selectedInterval.ToUpper(), out interval))
-            {
-                return;
-            }
-            switch (interval)
-            {
-                // [DAILY] - StartBoundary(DateTime), DaysInterval(int), stopFlag(bool)
-                case ExeInterval.DAILY: // 
-                    taskDefinition.Triggers.Add(CreateDailyTrigger(stopFlag));
-                    break;
-
-                // [WEEKLY] - StartBoundray(DateTime), DaysOfWeek(DaysOfTheWeek), WeeksInterval(int), stopFlag(bool)
-                case ExeInterval.WEEKLY:
-                    taskDefinition.Triggers.Add(CreateWeeklyTrigger(stopFlag));
-                    break;
-
-                // [MONTHLY] - StartBoundary(DateTime), DaysOfWeek(DaysOfTheWeek), MonthsOfYear(MonthsOfTheYear), stopFlag(bool)
-                case ExeInterval.MONTHLY: // Type : Day
-                    // starts tomorrow, triggers on the first day of every month, doesn't run on the last day of the month
-                    
-                    taskDefinition.Triggers.Add(CreateMonthlyTrigger(stopFlag));
-                    
-                    //// Type : Weekday
-                    ////OR starts tomorrow, triggers on the first week's monday of every month
-                    //taskDefinition.Triggers.Add(CreateMonthlyTrigger2());
-                    break;
-            }
-            
+            taskDefinition.Triggers.Add(startTrigger);
             taskDefinition.Actions.Add(CreateExeAction(arguments));
             // Register the task in the root folder of the local machine
             TaskService.Instance.RootFolder.RegisterTaskDefinition("CogAplex Log Management System", taskDefinition);
@@ -69,8 +40,36 @@ namespace LogManagementSystem
             return CogAplex;
         }
 
+        //TODO-switch method
+        public static Trigger SelectTrigger(string interval, bool stopFlag = true)
+        {
+            ExeInterval triggerInterval;
+            if (!Enum.TryParse<ExeInterval>(interval.ToUpper(), out triggerInterval))
+            {
+                return null;
+            }
+
+            switch (triggerInterval)
+            {
+                case ExeInterval.DAILY: 
+                    return CreateDailyTrigger(stopFlag);
+
+                case ExeInterval.WEEKLY:
+                    return CreateWeeklyTrigger(stopFlag);
+
+                case ExeInterval.MONTHLY: 
+                    return CreateMonthlyTrigger(stopFlag);
+                    //return CreateMonthlyTrigger2(stopFlag);
+
+                default:
+                    return CreateDailyTrigger(stopFlag);
+            }
+
+        }
+
         public static DailyTrigger CreateDailyTrigger(bool stopFlag = true)
         {
+            // [DAILY] - StartBoundary(DateTime), DaysInterval(int), stopFlag(bool)
             DailyTrigger dailyTrigger = new DailyTrigger();
             dailyTrigger.StartBoundary = DateTime.Today.AddDays(1);
             dailyTrigger.DaysInterval = 1;
@@ -83,6 +82,7 @@ namespace LogManagementSystem
 
         public static WeeklyTrigger CreateWeeklyTrigger(bool stopFlag = true)
         {
+            // [WEEKLY] - StartBoundray(DateTime), DaysOfWeek(DaysOfTheWeek), WeeksInterval(int), stopFlag(bool)
             WeeklyTrigger weeklyTrigger = new WeeklyTrigger();
             weeklyTrigger.StartBoundary = DateTime.Today.AddDays(1);
             weeklyTrigger.DaysOfWeek = DaysOfTheWeek.Monday;
@@ -96,6 +96,10 @@ namespace LogManagementSystem
 
         public static MonthlyTrigger CreateMonthlyTrigger(bool stopFlag = true)
         {
+            // [MONTHLY] - StartBoundary(DateTime), DaysOfWeek(DaysOfTheWeek), MonthsOfYear(MonthsOfTheYear), stopFlag(bool)
+            // Type : Day
+            // starts tomorrow, triggers on the first day of every month, doesn't run on the last day of the month
+
             MonthlyTrigger monthlyTrigger = new MonthlyTrigger();
             monthlyTrigger.StartBoundary = DateTime.Today.AddDays(1);
             monthlyTrigger.DaysOfMonth = new int[] { 1 };
@@ -110,6 +114,10 @@ namespace LogManagementSystem
 
         public static MonthlyDOWTrigger CreateMonthlyTrigger2(bool stopFlag = true)
         {
+            // [MONTHLY] - StartBoundary(DateTime), DaysOfWeek(DaysOfTheWeek), MonthsOfYear(MonthsOfTheYear), stopFlag(bool)
+            //// Type : Weekday
+            ////OR starts tomorrow, triggers on the first week's monday of every month
+
             MonthlyDOWTrigger monthlyTrigger = new MonthlyDOWTrigger();
             monthlyTrigger.StartBoundary = DateTime.Now.AddSeconds(2);
             monthlyTrigger.DaysOfWeek = DaysOfTheWeek.Monday;
@@ -122,6 +130,7 @@ namespace LogManagementSystem
 
             return monthlyTrigger;
         }
+
 
         public static void DeleteTaskSchedule(string deleteFlag)
         {
