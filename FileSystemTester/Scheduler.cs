4865f91de3ba6df94b1203788f23f273c36d7da5
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-06-29 16:37:14 +0900

    Simplified Class Instance Initialization

--- FileSystemTester/Scheduler.cs
+++ FileSystemTester/Scheduler.cs
@@ -11,18 +11,27 @@ namespace LogManagementSystem
         public string ZipDaysAfterLogged { get; private set; }
         public string DeleteDaysAfterZip { get; private set; }
         public string ExecutionInterval { get; private set; }
+        public DateTime StartExeFile { get; private set; }
+        public int ExeRepititionInterval { get; private set; }
 
-        public Scheduler(string rootPath, string zipDaysAfterLogged, string deleteDaysAfterZip, string exeInterval)
+        public Scheduler()
+        {
+
+        }
+
+        public Scheduler(
+            string rootPath, 
+            string zipDaysAfterLogged, 
+            string deleteDaysAfterZip, 
+            string exeInterval, 
+            DateTime? startExeFile = null)
         {
             RootPath = rootPath;
             ZipDaysAfterLogged = zipDaysAfterLogged;
             DeleteDaysAfterZip = deleteDaysAfterZip;
             ExecutionInterval = exeInterval;
-        }
-
-        public Scheduler()
-        {
-
+            StartExeFile = startExeFile ?? DateTime.Today.AddDays(1);
+            ExeRepititionInterval = 1;
         }
 
         public void AddTaskSchedule(Trigger startTrigger)
@@ -32,126 +41,106 @@ namespace LogManagementSystem
             taskDefinition.RegistrationInfo.Description = "Management of daily .log and captured image files.";
             taskDefinition.Triggers.Add(startTrigger);
             taskDefinition.Actions.Add(CreateExeAction());
-            // Register the task in the root folder of the local machine
             TaskService.Instance.RootFolder.RegisterTaskDefinition("CogAplex Log Management System", taskDefinition);
         }
 
         private ExecAction CreateExeAction()
         {
-            ExecAction CogAplex = new ExecAction();
-            CogAplex.Path = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            CogAplex.Arguments = $"zip {RootPath} {ZipDaysAfterLogged} {DeleteDaysAfterZip}";
+            ExecAction CogAplex = new ExecAction
+            {
+                Path = System.Reflection.Assembly.GetExecutingAssembly().Location,
+                Arguments = $"zip {RootPath} {ZipDaysAfterLogged} {DeleteDaysAfterZip}"
+            };
             return CogAplex;
         }
 
-        //public DailyTrigger SelectTrigger(string interval, 
-        //    DateTime? startingBoundary = null)
-        //{
-        //    if (interval.ToLower() == "daily")
-        //    {
-        //        return CreateDailyTrigger(startingBoundary);
-        //    }
-        //    return null;
-        //}
+        public DailyTrigger SelectTrigger()
+        {
+            if (ExecutionInterval.ToLower() == "daily")
+            {
+                return CreateDailyTrigger();
+            }
+            return null;
+        }
 
-        //public WeeklyTrigger SelectTrigger(string interval, 
-        //    DateTime? startingBoundary = null,
-        //    DaysOfTheWeek dayofWeek = DaysOfTheWeek.Monday)
-        //{
-        //    if (interval.ToLower() == "weekly")
-        //    {
-        //        return CreateWeeklyTrigger(startingBoundary, dayofWeek);
-        //    }
-        //    return null;
-        //}
+        public WeeklyTrigger SelectTrigger(
+            DaysOfTheWeek dayofWeek = DaysOfTheWeek.Monday)
+        {
+            if (ExecutionInterval.ToLower() == "weekly")
+            {
+                return CreateWeeklyTrigger(dayofWeek);
+            }
+            return null;
+        }
 
-        //public MonthlyTrigger SelectTrigger(string interval, 
-        //    DateTime? startingBoundary = null, 
-        //    int day = 1, 
-        //    MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths)
-        //{
-        //    if (interval.ToLower() == "monthly")
-        //    {
-        //        return CreateMonthlyTrigger(startingBoundary, day, monthsOfTheYear);
-        //    }
-        //    return null;
-        //}
+        public MonthlyTrigger SelectTrigger(
+            int dayOfMonth = 1, 
+            MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths)
+        {
+            if (ExecutionInterval.ToLower() == "monthly")
+            {
+                return CreateMonthlyTrigger(dayOfMonth, monthsOfTheYear);
+            }
+            return null;
+        }
 
-        //public MonthlyDOWTrigger SelectTrigger(string interval, 
-        //    DateTime? startingBoundary = null, 
-        //    DaysOfTheWeek daysOfTheWeek = DaysOfTheWeek.Monday, 
-        //    MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths,
-        //    WhichWeek whichweek = WhichWeek.AllWeeks)
-        //{
-        //    if (interval.ToLower() == "monthly2")
-        //    {
-        //        return CreateMonthlyTrigger2(startingBoundary, daysOfTheWeek, monthsOfTheYear, whichweek);
-        //    }
-        //    return null;
-        //}
-
-        public Trigger SelectTrigger(DateTime? startingBoundary = null, int daysInterval = 1,
-            DaysOfTheWeek dayofWeek = DaysOfTheWeek.Monday, int weeksInterval = 1,
-            int day = 1, MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths,
+        public Trigger CreateTrigger(
             DaysOfTheWeek daysOfTheWeek = DaysOfTheWeek.Monday,
-            WhichWeek whichweek = WhichWeek.AllWeeks
+            int dayOfMonth = 1, 
+            MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths
             )
         {
-            switch (ExecutionInterval.ToLower()) // enum말고 string 으로 switch할 것.
+            switch (ExecutionInterval.ToLower())
             {
                 case "daily":
-                    return CreateDailyTrigger(startingBoundary);
+                    return CreateDailyTrigger();
 
                 case "weekly":
-                    return CreateWeeklyTrigger(startingBoundary, dayofWeek);
+                    return CreateWeeklyTrigger(daysOfTheWeek);
 
                 case "monthly":
-                    return CreateMonthlyTrigger(startingBoundary, day, monthsOfTheYear);
-                //return CreateMonthlyTrigger2(startingBoundary, daysOfTheWeek, monthsOfTheYear, whichweek);
+                    return CreateMonthlyTrigger(dayOfMonth, monthsOfTheYear);
                 default:
-                    return CreateDailyTrigger(startingBoundary);
+                    return CreateDailyTrigger();
             }
         }
 
-        private DailyTrigger CreateDailyTrigger(
-            DateTime? startBoundary = null)
+        private DailyTrigger CreateDailyTrigger()
         {
-            // [DAILY] - StartBoundary(DateTime), DaysInterval(int), stopFlag(bool)
-            DailyTrigger dailyTrigger = new DailyTrigger();
-            dailyTrigger.StartBoundary = startBoundary ?? DateTime.Today.AddDays(1);
-            dailyTrigger.DaysInterval = 1;
+            DailyTrigger dailyTrigger = new DailyTrigger
+            {
+                StartBoundary = StartExeFile,
+                DaysInterval = (short)ExeRepititionInterval
+            };
             return dailyTrigger;
         }
 
         private WeeklyTrigger CreateWeeklyTrigger(
-            DateTime? startBoundary = null, 
-            DaysOfTheWeek dayofWeek = DaysOfTheWeek.Monday)
+            DaysOfTheWeek daysOfTheWeek = DaysOfTheWeek.Monday)
         {
-            // [WEEKLY] - StartBoundray(DateTime), DaysOfWeek(DaysOfTheWeek), WeeksInterval(int), stopFlag(bool)
-            WeeklyTrigger weeklyTrigger = new WeeklyTrigger();
-            weeklyTrigger.StartBoundary = startBoundary ?? DateTime.Today.AddDays(1);
-            weeklyTrigger.DaysOfWeek = DaysOfTheWeek.Monday;
-            weeklyTrigger.WeeksInterval = 1;
+            WeeklyTrigger weeklyTrigger = new WeeklyTrigger
+            {
+                StartBoundary = StartExeFile,
+                DaysOfWeek = daysOfTheWeek,
+                WeeksInterval = (short)ExeRepititionInterval
+            };
             return weeklyTrigger;
         }
 
         private MonthlyTrigger CreateMonthlyTrigger(
-            DateTime? startBoundary = null, 
             int day = 1, 
             MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths)
         {
-            // [MONTHLY] - StartBoundary(DateTime), DaysOfWeek(DaysOfTheWeek), MonthsOfYear(MonthsOfTheYear), stopFlag(bool)
-            // Type : Day
-            // starts tomorrow, triggers on the first day of every month, doesn't run on the last day of the month
-
-            MonthlyTrigger monthlyTrigger = new MonthlyTrigger();
-            monthlyTrigger.StartBoundary = startBoundary ?? DateTime.Today.AddDays(1);
-            monthlyTrigger.DaysOfMonth = setDaysToMonthlyTrigger(day);
-            monthlyTrigger.MonthsOfYear = monthsOfTheYear;
-            monthlyTrigger.RunOnLastDayOfMonth = false; // V2 only
+            MonthlyTrigger monthlyTrigger = new MonthlyTrigger
+            {
+                StartBoundary = StartExeFile,
+                DaysOfMonth = setDayOfMonth(day),
+                MonthsOfYear = monthsOfTheYear,
+                RunOnLastDayOfMonth = false
+            };
             return monthlyTrigger;
         }
-        private int[] setDaysToMonthlyTrigger(int day = 1)
+        private int[] setDayOfMonth(int day = 1)
         {
             List<int> dayToSet = new List<int>();
             if (day > 0 && day <= 31)
@@ -159,25 +148,6 @@ namespace LogManagementSystem
                 dayToSet.Add(day);
             }
             return dayToSet.ToArray();
-        }
-
-
-        private MonthlyDOWTrigger CreateMonthlyTrigger2(
-            DateTime? startBoundary = null,
-            DaysOfTheWeek daysOfTheWeek = DaysOfTheWeek.Monday, 
-            MonthsOfTheYear monthsOfTheYear = MonthsOfTheYear.AllMonths,
-            WhichWeek whichweek = WhichWeek.AllWeeks)
-        {
-            // [MONTHLY] - StartBoundary(DateTime), DaysOfWeek(DaysOfTheWeek), MonthsOfYear(MonthsOfTheYear), stopFlag(bool)
-            //// Type : Weekday
-            ////OR starts tomorrow, triggers on the first week's monday of every month
-
-            MonthlyDOWTrigger monthlyTrigger = new MonthlyDOWTrigger();
-            monthlyTrigger.StartBoundary = startBoundary ?? DateTime.Today.AddDays(1);
-            monthlyTrigger.DaysOfWeek = daysOfTheWeek;
-            monthlyTrigger.MonthsOfYear = monthsOfTheYear;
-            monthlyTrigger.WeeksOfMonth = whichweek;
-            return monthlyTrigger;
         }
 
         public void DeleteTaskSchedule()
@@ -189,6 +159,16 @@ namespace LogManagementSystem
                     service.RootFolder.DeleteTask(item.Name, false);
                 }
             }
+        }
+
+        public void WeekdaySelector(string weekday)
+        {
+
+        }
+
+        public void MonthSelector(string month)
+        {
+
         }
     }
 }
