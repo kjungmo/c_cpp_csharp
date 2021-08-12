fbdfde9c6ea6b74bf9d42abb1dc39e52080a694d
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-08-12 13:09:31 +0900

    Corrects Solution structure

--- TcpConnection/TcpServer/Properties/AssemblyInfo.cs
+++ TcpConnection/TcpServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("TcpConnection")]
+[assembly: AssemblyTitle("TcpServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TcpConnection")]
+[assembly: AssemblyProduct("TcpServer")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("19ff67c8-5be3-441d-9ad7-f3369b1916ce")]
+[assembly: Guid("61dc4c27-4522-4b46-827c-62a77292689e")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
