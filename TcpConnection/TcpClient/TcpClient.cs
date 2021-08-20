db5e7a88e4f33edb222f9aa11cd5c78e54f01340
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-08-20 11:40:21 +0900

    Changes and rearranges codes

---
+++ TcpConnection/TcpClient/TcpClient.cs
@@ -0,0 +1,86 @@
+﻿using CommonUtils;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpConnection
+{
+    class TcpClient
+    {
+        private static readonly string _deviceName = ConfigurationManager.AppSettings["DeviceName"];
+        private static readonly string _startPosition = ConfigurationManager.AppSettings["StartPos"];
+        private static readonly string _ipAddress = ConfigurationManager.AppSettings["IPAddress"];
+        private static MelsecPLCTransferer _plc = new MelsecPLCTransferer();
+        static void Main()
+        {
+            if (!IPAddress.TryParse(_ipAddress, out IPAddress _))
+            {
+                return;
+            }
+            if (!int.TryParse(ConfigurationManager.AppSettings["Port"], out int port))
+            {
+                return;
+            }
+            if (port < 1024 || 49151 < port)
+            {
+                return;
+            }
+
+
+            if (!_plc.Init())
+            {
+                return;
+            }
+
+            TcpClient client = null;
+            NetworkStream ns = null;
+            StreamReader sr = null;
+            StreamWriter sw = null;
+
+            while (true)
+            {
+                try
+                {
+                    client = new TcpClient(_ipAddress, port);
+                    ns = client.GetStream();
+                    sr = new StreamReader(ns); // **********
+                    sw = new StreamWriter(ns);
+                    Console.WriteLine(sr.ReadLine()); // **********
+                    while (true)
+                    {
+                        ReadPlcWriteToStream(sw);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                finally
+                {
+                    sr?.Close();
+                    sw?.Close();
+                    ns?.Close();
+                    client?.Close();
+                }
+            }
+        }
+
+        private static void ReadPlcWriteToStream(StreamWriter writer)
+        {
+            (int ret, int[] resArray) = _plc.ReadDeviceBlock(_deviceName, _startPosition, 1);
+            if (ret == 0)
+            {
+                System.Threading.Thread.Sleep(100);
+                writer.WriteLine(resArray[0]);
+                writer.Flush();
+                Console.WriteLine(resArray[0]);
+            }
+            else
+            {
+                Console.WriteLine("Check for PLC connection.");
+            }
+        }
+    }
+}
---
+++ TcpConnection/TcpServer/TcpServer.cs
@@ -0,0 +1,96 @@
+﻿using CommonUtils;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpConnection
+{
+    class TcpServer
+    {
+        private static readonly string _deviceName = ConfigurationManager.AppSettings["DeviceName"];
+        private static readonly string _startPosition = ConfigurationManager.AppSettings["StartPos"];
+        private static MelsecPLCTransferer _plc = new MelsecPLCTransferer();
+        static void Main()
+        {
+            if (!int.TryParse(ConfigurationManager.AppSettings["Port"], out int port))
+            {
+                return;
+            }
+            if (port < 1024 || 49151 < port)
+            {
+                return;
+            }
+
+            if (!_plc.Init())
+            {
+                return;
+            }
+
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            try
+            {
+                server.Start();
+            }
+            catch (SocketException)
+            {
+                return;
+            }
+
+            Console.WriteLine("Server started"); //  ****************
+
+            NetworkStream ns = null;
+            StreamReader sr = null;
+            StreamWriter sw = null;
+            TcpClient client = null;
+
+            while (true)
+            {
+                try
+                {
+                    client = server.AcceptTcpClient();
+                    Console.WriteLine("Waiting for a connection..."); //  ****************
+                    ns = client.GetStream();
+                    sr = new StreamReader(ns);
+                    sw = new StreamWriter(ns); //  **************************
+                    Console.WriteLine("Client connected!"); //  ****************
+                    string welcome = "Server Connnect Success!";  //  ****************
+                    sw.WriteLine(welcome); //  ****************
+                    sw.Flush(); //  ****************
+                    while (true)
+                    {
+                        ReadStreamWriteToPlc(sr);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                finally
+                {
+                    ns?.Close();
+                    sr?.Close();
+                    sw?.Close();
+                    client?.Close();
+                }
+            }
+        }
+
+        private static void ReadStreamWriteToPlc(StreamReader reader)
+        {
+            string fromStream = reader.ReadLine();
+            if (!string.IsNullOrEmpty(fromStream) && int.TryParse(fromStream, out int data))
+            {
+                if (_plc.WriteDeviceBlock(_deviceName, _startPosition, new int[] { data }) == 0)
+                {
+                    Console.WriteLine($"Writing : [{data}]");
+                }
+                else
+                {
+                    Console.WriteLine("Failed writing. PLC disconnected.");
+                }
+            }
+        }
+    }
+}
