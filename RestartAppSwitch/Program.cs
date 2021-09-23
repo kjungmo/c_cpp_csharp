dc89480aece3caa31c6f319b216d8321a9f43952
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-09-23 19:52:17 +0900

    adds restarter

---
+++ RestartAppSwitch/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RestartAppSwitch
+{
+    internal class Program
+    {
+        private static readonly string s_fineLocalizer = "FineLocalizer";
+
+        private static void Main(string[] args)
+        {
+            if (args.Length > 2 && args[1] == "restart")
+            {
+                RestartProcess(s_fineLocalizer);
+            }
+            else
+            {
+                StartProcess(s_fineLocalizer);
+            }
+        }
+
+        private static void RestartProcess(string processName)
+        {
+            KillProcess(processName);
+            StartProcess(processName);
+        }
+
+        private static void KillProcess(string processName)
+        {
+            Process[] fineLocalizer = Process.GetProcessesByName(processName);
+            try
+            {
+                if (fineLocalizer.Length > 0)
+                {
+                    fineLocalizer[0].Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+
+        private static void StartProcess(string processName)
+        {
+            Process[] fineLocalizer = Process.GetProcessesByName(processName);
+            try
+            {
+                if (fineLocalizer.Length < 1)
+                {
+                    string procPath = Path.Combine(Environment.CurrentDirectory, $"{processName}.exe");
+                    if (!File.Exists(procPath))
+                    {
+                        Console.WriteLine($"We need {processName}.exe in the same directory");
+                        return;
+                    }
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = procPath;
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+                    process.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
