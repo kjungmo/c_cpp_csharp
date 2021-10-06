77d1720d9d54f9cbb82f37440b3227bd088da4ba
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-10-06 14:57:29 +0900

    Csharpener with Python

---
+++ PythonCSharpener/CSharpener.py
@@ -0,0 +1,17 @@
+#-*- coding:utf-8 -*-
+file1 = open('FineLocalizerForm2.cs', 'r', encoding='utf-8')
+appliedCode = ''
+while True:
+    line = file1.read()
+    if not line:
+        break
+    line = re.sub('"!@(\w+)"', r'Lang.Msg.\1', line, flags=re.MULTILINE)
+    appliedCode = line
+    #print('Line {}\n:\n{}'.format(count, line))
+print('****************')
+print('appliedCode HERE')
+print(appliedCode)
+file1 = open('FineLocalizerForm2.cs','w+',encoding='utf-8')
+file1.write(appliedCode)
+file1.close()
+print("regex applied\n")
