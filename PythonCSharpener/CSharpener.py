e90703fa9848a38e80e7098570cc95b75ce75e93
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-10-07 13:50:56 +0900

    updated code

--- PythonCSharpener/CSharpener.py
+++ PythonCSharpener/CSharpener.py
@@ -1,18 +1,29 @@
 #-*- coding:utf-8 -*-
 import re
-file1 = open('FineLocalizerForm2.cs', 'r', encoding='utf-8')
-appliedCode = ''
-while True:
-    line = file1.read()
-    if not line:
-        break
-    line = re.sub('"!@(\w+)"', r'Lang.Msg.\1', line, flags=re.MULTILINE)
-    appliedCode = line
-    #print('Line {}\n:\n{}'.format(count, line))
-print('****************')
-print('appliedCode HERE')
-print(appliedCode)
-file1 = open('FineLocalizerForm2.cs','w+',encoding='utf-8')
-file1.write(appliedCode)
-file1.close()
-print("regex applied\n")
+import os
+import sys
+
+def tokenize_localization_key_by_regex(filename):
+    if not os.path.exists(filename):
+        print('no .cs file')
+        return
+
+    file = open(filename, 'r', encoding='utf-8')
+    appliedCode = ''
+    while True:
+        line = file.read()
+        if not line:
+            break
+        line = re.sub('"!@(\w+)"', r'Lang.Msg.\1', line, flags=re.MULTILINE)
+        appliedCode = line
+    file.close()
+    file = open(filename, 'w+', encoding='utf-8')
+    file.write(appliedCode)
+    file.close()
+    print("regex applied\n")
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        tokenize_localization_key_by_regex(sys.argv[1])
+    else:
+        print("Usage : python3 CSharpener.py filename.cs")
