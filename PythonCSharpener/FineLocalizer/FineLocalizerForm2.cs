ff59902182a940b9e9672533bb0d2469bfd5af31
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-10-12 18:20:52 +0900

    Testing github action

--- PythonCSharpener/FineLocalizer/FineLocalizerForm2.cs
+++ PythonCSharpener/FineLocalizer/FineLocalizerForm2.cs
@@ -629,7 +629,7 @@ namespace FineLocalizer
 
                     RefPosesFileManager.SaveToFile(_refPoses, $"{_config[-1].VehicleRefDataPath}/ref_pose.dat", _config[-1].VehicleCamera);
 
-                    Logger.Info($"{"!@SavingRefPoseDone"} (point= {pointNum})");
+                    Logger.Info($"{Lang.MsgBoxFineLo.SavingRefPoseDone} (point= {pointNum})");
                 }
                 catch (Exception ex)
                 {
