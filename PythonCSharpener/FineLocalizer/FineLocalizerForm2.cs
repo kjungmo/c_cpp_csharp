6471ae026e21a60933de0ebfb01d4347d15252b4
kjungmo <79957698+jmokang@users.noreply.github.com>
2021-10-13 11:00:45 +0900

    Testing github action

--- PythonCSharpener/FineLocalizer/FineLocalizerForm2.cs
+++ PythonCSharpener/FineLocalizer/FineLocalizerForm2.cs
@@ -1675,7 +1675,7 @@ namespace FineLocalizer
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"{Lang.Msgs.InvalidPath} {ex}", Lang.MsgBoxFineLo.WarningTitle);
+                MessageBox.Show($"{"!@InvalidPath"} {ex}", Lang.MsgBoxFineLo.WarningTitle);
                 tb.Text = _sConfAcc.Get(_config[-1], tb.Name.Substring(2)).ToString();
                 return;
             }
